bb990c0248e5fe4d3735d36c85b44e7b5d21cb19
Md. Rahat Khan <65401897+Md-Rahat-Khan@users.noreply.github.com>
2025-10-09 00:44:19 +0600

    Update main.py

--- Predect Output/main.py
+++ Predect Output/main.py
@@ -1,33 +1,37 @@
 import pandas as pd
-from sklearn import tree
-from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 import numpy as np
 
-#le = preprocessing.LabelEncoder()
 df = pd.read_csv('RFID.csv')
-#df.head()
 print(df)
+
 x = df.drop(columns=['Result'])
-#print(x)
 inputs = x.drop(columns=['Case No.'])
-#inputx = inputs.to_numpy()
-#inputy = np.asarray(inputs)
-#print(inputy)
-#print(inputx)
 print(inputs)
-#target = df['Result']
+
 target = df['Result']
-#targetx =target.to_numpy()
-#targety = np.asarray(target)
-#print(targety)
-#print(targetx)
 print(target)
 
-model = tree.DecisionTreeClassifier()
-model.fit(inputs, target)
+# Split the data into 70% training and 30% testing
+X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.3, random_state=42)
 
-scores = model.score(inputs, target)
-print('score:'+str(scores))
+# Create and train Random Forest Classifier with enhanced hyperparameters
+model = RandomForestClassifier(
+    n_estimators=100,
+    max_depth=10,
+    min_samples_split=5,
+    min_samples_leaf=2,
+    random_state=42
+)
+model.fit(X_train, y_train)
+
+# Calculate scores on both training and test sets
+train_score = model.score(X_train, y_train)
+test_score = model.score(X_test, y_test)
+
+print('Training score: '+str(train_score))
+print('Testing score: '+str(test_score))
 
 a = input('enter 1st roads density between 0-3: ')
 b = input('enter 2nd roads density between 0-3: ')
@@ -37,5 +41,3 @@ e = input('Enter RFID number between 0-4: ')
 
 pre = model.predict([[a, b, c, d, e]])
 print('output='+str(pre))
-
-
